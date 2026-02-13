ca9b577e911310978666ec9cb50e6e076d1d9ed6
Myth <myth@gmail.com>
2026-02-13 18:10:44 +0000

    Add frontend_url terraform output

--- infrastructure/aws/prod/outputs.tf
+++ infrastructure/aws/prod/outputs.tf
@@ -1,12 +1,8 @@
-output "alb_dns" {
-  value = aws_lb.main.dns_name
+output "frontend_url" {
+  value = "http://${aws_lb.main.dns_name}"
 }
 
 output "backend_url" {
   value = "http://${aws_lb.main.dns_name}/api/health"
-}
-
-output "frontend_url" {
-  value = "http://${aws_lb.main.dns_name}"
 }
 
