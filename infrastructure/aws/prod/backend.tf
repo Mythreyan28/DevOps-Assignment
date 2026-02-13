70ca1281b93814112ffcc7a1e99427dca9e5327d
Myth <myth@gmail.com>
2026-02-13 16:15:37 +0000

    Fix backend region and add debug step

--- infrastructure/aws/prod/backend.tf
+++ infrastructure/aws/prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "devops-tf-state-1770900240"
     key            = "prod/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
   }
